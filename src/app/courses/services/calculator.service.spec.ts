c59624a1e7fbc3bade23391390b14fba15fece04
Rajkamal94 <76579542+Rajkamal94@users.noreply.github.com>
2020-12-26 17:20:17 +0530

    Update calculator.service.spec.ts

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -8,11 +8,11 @@ describe('CalculatorService', () => {
     let calculator: CalculatorService,
         loggerSpy: any;
 
-    beforeEach(()=> {
+    beforeEach(() => {
 
-        console.log("Calling beforeEach");
+        console.log('Calling beforeEach');
 
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
 
         TestBed.configureTestingModule({
             providers: [
@@ -21,13 +21,13 @@ describe('CalculatorService', () => {
             ]
         });
 
-        calculator = TestBed.get(CalculatorService);
+        calculator = TestBed.inject(CalculatorService);
 
     });
 
     it('should add two numbers', () => {
 
-        console.log("add test");
+        console.log('add test');
 
         const result = calculator.add(2, 2);
 
@@ -40,11 +40,11 @@ describe('CalculatorService', () => {
 
     it('should subtract two numbers', () => {
 
-        console.log("subtract test");
+        console.log('subtract test');
 
         const result = calculator.subtract(2, 2);
 
-        expect(result).toBe(0, "unexpected subtraction result");
+        expect(result).toBe(0, 'unexpected subtraction result');
 
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
